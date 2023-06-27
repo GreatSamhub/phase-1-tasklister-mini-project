6e8d546b26409d6633634b18b4c61c4984b007d1
+Sam Thongo <+samuelthongo73@gmail.com>
2023-06-27 16:04:04 +0300

    tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.getElementById('create-task-form')
+  const taskLists = document.getElementById('tasks')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    taskDescription = (e.target["new-task-description"].value)
+    if (taskDescription) {
+      createToDoList(taskDescription)
+      form.reset()
+    }
+  });
+  function createToDoList(description) {
+    const listItem = document.createElement('li');
+    listItem.textContent = description;
+    taskLists.appendChild(listItem);
+  }  
+})
